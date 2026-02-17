e3f3ef9da547fd564a38fcb9195b063b82f588a3
unknown <alamine2210@gmail.com>
2026-02-17 14:53:23 +0000

    Derniere version du portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <footer className="py-10 text-center border-t border-blue-950 text-sm text-zinc-500">
-      <p>© {new Date().getFullYear()} Al Amine Sao</p>
+      <p>© {new Date().getFullYear()} TECHAMINE</p>
       <p className="mt-2">
-        Portfolio personnel — Tous droits réservés
+        Portfolio Professionnel — Tous droits réservés
       </p>
     </footer>
   );
